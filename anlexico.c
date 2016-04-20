0ac5f645cfee3bcae5ce8ee5613a8a71bc44a43b
Luis Méndez <luis.rains@hotmail.com>
2016-04-20 15:30:46 -0400

    2.1

---
+++ anlexico.c
@@ -0,0 +1,462 @@
+/*
+ *	Analizador L�xico	
+ *	Curso: Compiladores y Lenguajes de Bajo de Nivel
+ *	Pr�ctica de Programaci�n Nro. 1
+ *	
+ *	Descripcion:
+ *	Implementa un analizador l�xico que reconoce n�meros, identificadores, 
+ * 	palabras reservadas, operadores y signos de puntuaci�n para un lenguaje
+ * 	con sintaxis tipo Pascal.
+ *	
+ */
+
+/*********** Inclusi�n de cabecera **************/
+#include "anlexico.h"
+
+
+/************* Variables globales **************/
+
+int consumir;			/* 1 indica al analizador lexico que debe devolver
+						el sgte componente lexico, 0 debe devolver el actual */
+
+char cad[5*TAMLEX];		// string utilizado para cargar mensajes de error
+token t;				// token global para recibir componentes del Analizador Lexico
+
+// variables para el analizador lexico
+
+FILE *archivo;			// Fuente pascal
+char buff[2*TAMBUFF];	// Buffer para lectura de archivo fuente
+char id[TAMLEX];		// Utilizado por el analizador lexico
+int delantero=-1;		// Utilizado por el analizador lexico
+int fin=0;				// Utilizado por el analizador lexico
+int numLinea=1;			// Numero de Linea
+int cont_esp=0;    //variabe para contar la cantidad de espacio
+
+/**************** Funciones **********************/
+
+
+// Rutinas del analizador lexico
+
+void error(const char* mensaje)
+{
+	printf("Lin %d: Error Lexico. %s.\n",numLinea,mensaje);	
+}
+
+void sigLex()
+{
+	int i=0;
+	char c=0;
+	int acepto=0;
+	int estado=0;
+	char msg[41];
+	entrada e;
+  
+
+	while((c=fgetc(archivo))!=EOF)
+	{
+		
+		if (c==' '){
+		    do{
+                        if(c==' ')
+                            cont_esp = cont_esp + 1;//contador de espacios para luego imprimir
+                            c=fgetc(archivo); 
+                    }while(c <> ' ');//ver si es un " HASTA QUE ENCUENTRE " O CARACTERO \N
+			//continue;	//eliminar espacios en blanco
+		}
+        else if (c=='\t')
+        { 
+            //si es un tabulador que guarde los 4 espacios correspondientes
+            while(c=='\t'){ 
+                agregar_espacio(); //funcion que agregara la cantidad de espacios de acuerdo a la cantidad de tabulaciones corridas
+		c=fgetc(archivo);
+            }
+        }
+        else if(c=='\n')
+		{
+			//incrementar el numero de linea
+			numLinea++;
+			continue;
+		}    
+		else if (isdigit(c))
+		{
+                    //es un numero
+			i=0;
+			estado=0;
+			acepto=0;
+			id[i]=c;
+				
+			while(!acepto)
+			{
+				switch(estado){
+				case 0: //una secuencia netamente de digitos, puede ocurrir . o e
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=0;
+						}
+						else if(c=='.'){
+							id[++i]=c;
+							estado=1;
+						}
+						else if(tolower(c)=='e'){
+							id[++i]=c;
+							estado=3;
+						}
+						else{
+							estado=6;
+						}
+						break;
+					
+					case 1://un punto, debe seguir un digito (caso especial de array, puede venir otro punto)
+						c=fgetc(archivo);						
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=2;
+						}
+						else if(c=='.')
+						{
+							i--;
+							fseek(archivo,-1,SEEK_CUR);
+							estado=6;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 2://la fraccion decimal, pueden seguir los digitos o e
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=2;
+						}
+						else if(tolower(c)=='e')
+						{
+							id[++i]=c;
+							estado=3;
+						}
+						else
+							estado=6;
+						break;
+					case 3://una e, puede seguir +, - o una secuencia de digitos
+						c=fgetc(archivo);
+						if (c=='+' || c=='-')
+						{
+							id[++i]=c;
+							estado=4;
+						}
+						else if(isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 4://necesariamente debe venir por lo menos un digito
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 5://una secuencia de digitos correspondiente al exponente
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							estado=6;
+						}break;
+					case 6://estado de aceptacion, devolver el caracter correspondiente a otro componente lexico
+						if (c!=EOF)
+							ungetc(c,archivo);
+						else
+							c=0;
+						id[++i]='\0';
+						acepto=1;
+						t.pe=buscar(id);
+						if (t.pe->compLex==-1)
+						{
+							strcpy(e.lexema,id);
+							e.compLex=NUM;
+							insertar(e);
+							t.pe=buscar(id);
+						}
+						t.compLex=NUM;
+						break;
+					case -1:
+						if (c==EOF)
+							error("No se esperaba el fin de archivo");
+						else
+							error(msg);
+						exit(1);
+					}
+				}
+			break;
+		}
+                else if (c=='\"')
+		{
+			//es un attribute-name o un attribute-value
+			i=0;
+			do{
+				id[i]=c;
+				i++;
+				c=fgetc(archivo);
+				if (i>=TAMLEX)
+					error("Longitud de Identificador excede tama�o de buffer");
+			}while(isalpha(c) || isdigit(c));
+			id[i]='\0';
+			if (c!=EOF)
+				ungetc(c,archivo);
+			else
+				c=0;
+			t.pe=buscar(id);
+			t.compLex=t.pe->compLex;
+			if (t.pe->compLex==-1)
+			{
+				strcpy(e.lexema,id);
+				e.compLex=ID;
+				insertar(e);
+				t.pe=buscar(id);
+				t.compLex=ID;
+			}
+			break;
+		}
+		else if (c==':')
+		{
+			//puede ser un : o un operador de asignacion
+			c=fgetc(archivo);
+			if (c=='='){
+				t.compLex=OPASIGNA;
+				t.pe=buscar(":=");
+			}
+			else{
+				ungetc(c,archivo);
+				t.compLex=':';
+				t.pe=buscar(":");
+			}
+			break;
+		}
+		else if (c=='+')
+		{
+			t.compLex=OPSUMA;
+			t.pe=buscar("+");
+			break;
+		}
+		else if (c=='-')
+		{
+			t.compLex=OPSUMA;
+			t.pe=buscar("-");
+			break;
+		}
+		else if (c=='*')
+		{
+			t.compLex=OPMULT;
+			t.pe=buscar("*");
+			break;
+		}
+		else if (c=='/')
+		{
+			t.compLex=OPMULT;
+			t.pe=buscar("/");
+			break;
+		}
+		else if (c=='=')
+		{
+			t.compLex=OPREL;
+			t.pe=buscar("=");
+			break;
+		}
+		else if (c==',')
+		{
+			t.compLex=',';
+			t.pe=buscar(",");
+			break;
+		}
+		else if (c==';')
+		{
+			t.compLex=';';
+			t.pe=buscar(";");
+			break;
+		}
+		else if (c=='.')
+		{
+			t.compLex='.';
+			t.pe=buscar(".");
+			break;
+		}
+		else if (c=='(')
+		{
+			if ((c=fgetc(archivo))=='*')
+			{//es un comentario
+				while(c!=EOF)
+				{
+					c=fgetc(archivo);
+					if (c=='*')
+					{
+						if ((c=fgetc(archivo))==')')
+						{
+							break;
+						}
+					}
+					else if(c=='\n')
+					{
+						//incrementar el numero de linea
+						numLinea++;
+					}
+				}
+				if (c==EOF)
+					error("Se llego al fin de archivo sin finalizar un comentario");
+				continue;
+			}
+			else
+			{
+				ungetc(c,archivo);
+				t.compLex='(';
+				t.pe=buscar("(");
+			}
+			break;
+		}
+		else if (c==')')
+		{
+			t.compLex=')';
+			t.pe=buscar(")");
+			break;
+		}
+		else if (c=='[')
+		{
+			t.compLex='[';
+			t.pe=buscar("[");
+			break;
+		}
+		else if (c==']')
+		{
+			t.compLex=']';
+			t.pe=buscar("]");
+			break;
+		}
+		else if (c=='\'')
+		{//un caracter o una cadena de caracteres
+			i=0;
+			id[i]=c;
+			i++;
+			do{
+				c=fgetc(archivo);
+				if (c=='\'')
+				{
+					c=fgetc(archivo);
+					if (c=='\'')
+					{
+						id[i]=c;
+						i++;
+						id[i]=c;
+						i++;
+					}
+					else
+					{
+						id[i]='\'';
+						i++;
+						break;
+					}
+				}
+				else if(c==EOF)
+				{
+					error("Se llego al fin de archivo sin finalizar un literal");
+				}
+				else{
+					id[i]=c;
+					i++;
+				}
+			}while(isascii(c));
+			id[i]='\0';
+			if (c!=EOF)
+				ungetc(c,archivo);
+			else
+				c=0;
+			t.pe=buscar(id);
+			t.compLex=t.pe->compLex;
+			if (t.pe->compLex==-1)
+			{
+				strcpy(e.lexema,id);
+				if (strlen(id)==3 || strcmp(id,"''''")==0)
+					e.compLex=CAR;
+				else
+					e.compLex=LITERAL;
+				insertar(e);
+				t.pe=buscar(id);
+				t.compLex=e.compLex;
+			}
+			break;
+		}
+		else if (c=='{')
+		{
+			//elimina el comentario
+			while(c!=EOF)
+			{
+				c=fgetc(archivo);
+				if (c=='}')
+					break;
+				else if(c=='\n')
+				{
+					//incrementar el numero de linea
+					numLinea++;
+				}
+			}
+			if (c==EOF)
+				error("Se llego al fin de archivo sin finalizar un comentario");
+		}
+		else if (c!=EOF)
+		{
+			sprintf(msg,"%c no esperado",c);
+			error(msg);
+		}
+	}
+	if (c==EOF)
+	{
+		t.compLex=EOF;
+		// strcpy(e.lexema,"EOF");
+		sprintf(e.lexema,"EOF");
+		t.pe=&e;
+	}
+	
+}
+
+int main(int argc,char* args[])
+{
+	// inicializar analizador lexico
+
+	initTabla();
+	initTablaSimbolos();
+	
+	if(argc > 1)
+	{
+		if (!(archivo=fopen(args[1],"rt")))
+		{
+			printf("Archivo no encontrado.\n");
+			exit(1);
+		}
+		while (t.compLex!=EOF){
+			sigLex();
+			printf("Lin %d: %s -> %d\n",numLinea,t.pe->lexema,t.compLex);
+		}
+		fclose(archivo);
+	}else{
+		printf("Debe pasar como parametro el path al archivo fuente.\n");
+		exit(1);
+	}
+
+	return 0;
+}
